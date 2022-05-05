51a38367b9d9859909f70885a4fd04b4b2647b12
ankitkrsingh <ankitkrsingh2012@gmail.com>
2022-05-05 20:33:04 +0200

    Tocontroller angepasst

--- src/main/java/com/example/springboot04022022/controller/ToDoController.java
+++ src/main/java/com/example/springboot04022022/controller/ToDoController.java
@@ -2,6 +2,9 @@ package com.example.springboot04022022.controller;
 
 import com.example.springboot04022022.entity.ToDo;
 import com.example.springboot04022022.service.ToDoService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class ToDoController {
 
-    @Autowired
     private ToDoService toDoService;
 
     @GetMapping("/todos")
